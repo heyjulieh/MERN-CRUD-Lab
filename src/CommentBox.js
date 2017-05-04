9e31ed27ae404b51a80447755a0b25285ae0219f
janeosaur <janewie@gmail.com>
2017-05-04 11:47:39 -0700

    finished sprint4, added server routes

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
-import DATA from '../data';
 import style from './style';
 
 class CommentBox extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: []
-    };
+    this.state = { data: [] };
   }
   render() {
     return (
       <div style={ style.commentBox }>
         <h2>Comments:</h2>
-      <CommentList data={ DATA }/>
+      <CommentList data={ this.state.data }/>
       <CommentForm />
       </div>
     )
